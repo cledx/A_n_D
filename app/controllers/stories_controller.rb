614831e578479b8a2d7ae450e839b28a6aea534e
Carlos Ledoux <carlosledoux@gmail.com>
2026-02-24 14:14:24 +0900

    Added Context to the Story Model

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,24 @@
 class StoriesController < ApplicationController
+ 
+  def new
+    @character = Character.find(params[:character_id])
+    @story = Story.new({
+      character_id: @character.id
+      health_points: 20,
+      title: "#{@character.name}'s Story",
+      summary: "",
+      level: 1
+    })
+  end
+
+  def create
+
+  end
+
+  private
+
+  def stories_params
+    params.require(:story).permit(:mood, :setting)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_24_030316) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_24_051335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,18 +36,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_24_030316) do
     t.index ["story_id"], name: "index_messages_on_story_id"
   end
 
-  create_table "queries", force: :cascade do |t|
-    t.string "code"
-    t.datetime "created_at", null: false
-    t.string "error"
-    t.string "history"
-    t.string "info"
-    t.string "language"
-    t.datetime "updated_at", null: false
-  end
-
   create_table "stories", force: :cascade do |t|
     t.bigint "character_id", null: false
+    t.text "context"
     t.datetime "created_at", null: false
     t.integer "health_points"
     t.integer "level"
