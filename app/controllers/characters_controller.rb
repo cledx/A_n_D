d0d968e136def965331b8472457d44248a35443e
Glaucia Grossi <glau.kobashigawa.jp@gmail.com>
2026-02-24 13:58:56 +0900

    added character controller: new, create, character_params

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,27 @@
 class CharactersController < ApplicationController
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+
+    if @character.save
+      redirect_to @character, notice: "Character created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(
+      :name,
+      :bio,
+      :character_class,
+      :gender,
+      :race
+    )
+  end
 end
