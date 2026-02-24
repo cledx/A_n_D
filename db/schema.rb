3aa80afa4505067739f27f61dae9e066a1de384d
Carlos Ledoux <carlosledoux@gmail.com>
2026-02-24 12:15:51 +0900

    Finished Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_23_085635) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_24_030316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_23_085635) do
     t.string "name"
     t.string "race"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -32,6 +34,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_23_085635) do
     t.bigint "story_id", null: false
     t.datetime "updated_at", null: false
     t.index ["story_id"], name: "index_messages_on_story_id"
+  end
+
+  create_table "queries", force: :cascade do |t|
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.string "error"
+    t.string "history"
+    t.string "info"
+    t.string "language"
+    t.datetime "updated_at", null: false
   end
 
   create_table "stories", force: :cascade do |t|
@@ -59,6 +71,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_23_085635) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "characters", "users"
   add_foreign_key "messages", "stories"
   add_foreign_key "stories", "characters"
 end
