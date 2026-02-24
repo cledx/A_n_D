b6056d11bf28f5cca06abf2ad4ee7dcef0030867
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-02-24 15:44:43 +0900

    fixed everything

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,5 +2,6 @@ class StoriesController < ApplicationController
   def index
     @stories = current_user.stories
     @characters = current_user.characters
+    @character = Character.find(params[:character_id])
   end
 end
