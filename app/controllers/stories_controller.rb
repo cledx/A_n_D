4c5c253b5a5843611419dff884111eafd91a109a
Katherine Rush <108463876+krushy8@users.noreply.github.com>
2026-02-25 15:26:48 +0900

    added styles

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,37 +1,35 @@
 class StoriesController < ApplicationController
-  before_action :find_character, only: [:new, :create]
+  before_action :find_character, only: %i[new create]
 
   def index
-    @stories = current_user.stories
     @characters = current_user.characters
-    @character = Character.find(params[:character_id])
+    @stories = current_user.stories
   end
-  
+
   def new
     @story = Story.new
   end
 
   def create
     @story = Story.new({
-      character_id: @character.id,
-      health_points: 20,
-      title: "#{@character.name}'s Story",
-      summary: "",
-      level: 1,
-      setting: params[:setting],
-      mood: params[:mood]
-    })
-    params[:context] ? @story.context = params[:context] : @story.context = ""
+                         character_id: @character.id,
+                         health_points: 20,
+                         title: "#{@character.name}'s Story",
+                         summary: "",
+                         level: 1,
+                         setting: params[:setting],
+                         mood: params[:mood]
+                       })
+    @story.context = params[:context] || ""
     if @story.save
       @message = Message.create({
-        story_id: @story.id,
-        content: "You just created this story"
-      })
+                                  story_id: @story.id,
+                                  content: "You just created this story"
+                                })
       redirect_to messages_path(@message)
     else
       render :new, status: :unprocessable_entity
     end
-
   end
 
   private
@@ -43,5 +41,4 @@ class StoriesController < ApplicationController
   def find_character
     @character = Character.find(params[:character_id])
   end
-
 end
