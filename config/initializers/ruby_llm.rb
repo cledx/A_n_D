b50b8ebd1dd8d714c53500fa84ad924191cad7b9
Carlos Ledoux <carlosledoux@gmail.com>
2026-02-26 16:46:18 +0900

    API Key Hotfix

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,5 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV["OPEN_AI"]
+  config.openai_api_key = ENV["OPENAI_API_KEY"]
   # config.openai_api_base = "https://models.inference.ai.azure.com"
   # ... see RubyLLM configuration guide for other models
 end
