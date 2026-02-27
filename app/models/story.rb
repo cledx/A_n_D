3d9a60a14321661733093540b28be1ad4969b3ea
Carlos Ledoux <carlosledoux@gmail.com>
2026-02-27 15:48:01 +0900

    Tweaked Story Prompts

--- app/models/story.rb
+++ app/models/story.rb
@@ -14,15 +14,17 @@ class Story < ApplicationRecord
         The user is your player, adventuring in your story.
         The user is a Level #{level} #{character.race} #{character.character_class}
         The story is set a #{mood} story set in a Fantasy #{setting}
-        Narrate the user through the adventure, presenting them with challenges along the way.
+        Narrate the user through the adventure, presenting them with challenges along the way. Do not be afraid to damage them. Whenever they would do an action that would hurt them, make them take damage.
         Return your response in the form of a JSON with the following keys:{
         story_text: The story text to be displayed to the user. Describe the scene and what the non-player characters within the scene are doing or saying. Write in the second-person.
         option_1: A recommended course of action for the player
         option_2: A second recommended course of action for the user
-        dice_roll: If the user has told you an action that would require a dice roll, return a number here with the difficulty (out of 20) of the dice roll. If the action does not require a dice roll, return with false. A JSON with a die roll should have Roll a Die as option_1 and option_2.
         health_change: If the user's actions or the scene have caused the user's health to change, return a positive or negative value here. If the player's JSON ever contains a health value of 0, they have died and the adventure is over.
         }
       PROMPT
+      
+      # Dice Roll Prompting, To Be Implemented
+      # dice_roll: If the user has told you an action that would require a dice roll, return a number here with the difficulty (out of 20) of the dice roll. If the action does not require a dice roll, return with false. A JSON with a die roll should have Roll a Die as option_1 and option_2.
   end
   
   def generate_story_option
